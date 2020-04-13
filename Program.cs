42e4e97d11dc388f1e8a142392e9604c1030fc86
Шахзод Джабборов <shahzod@shah.local>
2020-04-14 02:28:58 +0500

    Version 2.0

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace XO_game
 {
     class Program
     {
-        static int[,] xoMap = {{0,0,0},{0,0,0},{0,0,0}};
+        static int[,] board = {{0,0,0},{0,0,0},{0,0,0}};
             //0 - nothing
             //1 - X
             //2 - O
@@ -12,11 +12,15 @@ namespace XO_game
         static bool flag = true; 
             //true - X move
             //false - O move
+        static int x, y, tempInput;
+        static bool[] boardHistory = new bool[9];
         public static void RestartGame()
         {
+            for (int i = 0; i < 9; i++)
+                boardHistory[i] = false;
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
-                    xoMap[i,j] = 0;
+                    board[i,j] = 0;
             flag = true;
             moves = 0;
         }
@@ -25,38 +29,86 @@ namespace XO_game
             int win = 0;
             for(int i = 0; i < 3; i++)
             {
-                if(xoMap[0,i]==xoMap[1,i] && xoMap[1,i]==xoMap[2,i] && xoMap[1,i] != 0)
-                    return xoMap[1,i];
-                if(xoMap[i,0]==xoMap[i,1] && xoMap[i,1]==xoMap[i,2] && xoMap[i,1] != 0)
-                    return xoMap[i,1];
+                if(board[0,i]==board[1,i] && board[1,i]==board[2,i] && board[1,i] != 0)
+                {
+                    board[0,i] += 2;
+                    board[1,i] += 2;
+                    board[2,i] += 2;
+                    return board[1,i];
+                }
+                if(board[i,0]==board[i,1] && board[i,1]==board[i,2] && board[i,1] != 0)
+                {
+                    board[i,0] += 2;
+                    board[i,1] += 2;
+                    board[i,2] += 2;
+                    return board[i,1];
+                }
             }
-            if(xoMap[0,0]==xoMap[1,1] && xoMap[1,1]==xoMap[2,2] && xoMap[1,1] != 0)
-                return xoMap[1,1];
-            if(xoMap[0,2]==xoMap[1,1] && xoMap[1,1]==xoMap[2,0] && xoMap[1,1] != 0)
-                return xoMap[1,1];
+            if(board[0,0]==board[1,1] && board[1,1]==board[2,2] && board[1,1] != 0)
+            {
+                board[0,0] += 2;
+                board[1,1] += 2;
+                board[2,2] += 2;
+                return board[1,1];
+            }
+            if(board[0,2]==board[1,1] && board[1,1]==board[2,0] && board[1,1] != 0)
+            {
+                board[0,2] += 2;
+                board[1,1] += 2;
+                board[2,0] += 2;
+                return board[1,1];
+            }
             return win;
             //0 - game continue
             //1 - X win
             //2 - O win
         }
-        public static void ShowMap()
+        public static void ShowBoard()
         {
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
-                    if(xoMap[i,j] == 0)
+                    if(board[i,j] == 0)
                         Console.Write("_ ");
-                    else if(xoMap[i,j] == 1)
+                    else if(board[i,j] == 1)
                         Console.Write("X ");
-                    else if(xoMap[i,j] == 2)
+                    else if(board[i,j] == 2)
                         Console.Write("O ");
+                    else if(board[i,j] == 9)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("X ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    else if(board[i,j] == 10)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("O ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
                 Console.WriteLine();
             }
             Console.WriteLine();
         }
+        public static void InputMove()
+        {
+            if(flag)
+                Console.WriteLine("Ход крестиков: ");
+            else Console.WriteLine("Ход ноликов: ");
+            if(!(int.TryParse(Console.ReadLine(), out tempInput)))
+            {
+                Console.WriteLine("Ошибка ввода, введите заново...");
+                InputMove();
+            }
+            for (int i = 0; i < 9; i++)
+                if(boardHistory[tempInput - 1] == true)
+                {
+                    Console.WriteLine("Ошибка, поле занято. Введите заново...");
+                    InputMove();
+                }
+        }
         static void Main()
         {
-            int x, y;
             do//Цикл для бесконечного повтора игры
             {
                 do//Цикл для принятия ходов
@@ -64,28 +116,27 @@ namespace XO_game
                     do//Цикл для проверки ходов
                     {
                         Console.Clear();
-                        ShowMap();
-                        if(flag)
-                            Console.WriteLine("Ход крестиков: ");
-                        else Console.WriteLine("Ход ноликов: ");
-                        int.TryParse(Console.ReadLine(), out x);
-                        int.TryParse(Console.ReadLine(), out y);
-                        x -= 1;
-                        y -= 1;
+                        ShowBoard();
+                        InputMove();
                     }
-                    while(0 > x || x > 2 || 0 > y || y > 2 || xoMap[x,y] != 0);
-                    xoMap[x,y] = flag ? 1 : 2;
-                    ShowMap();
+                    while(tempInput < 1 || 9 < tempInput);
+                    x = (tempInput - 1) / 3;
+                    y = (tempInput - 1) % 3;
+                    boardHistory[tempInput - 1] = true;
+                    board[x,y] = flag ? 1 : 2;
+                    ShowBoard();
                     moves++;
                     flag = !flag;
                 }
                 while(GameWinner()==0 && moves < 9);
                 if(GameWinner() == 0)
                     Console.WriteLine("Ничья");
-                else if(GameWinner() == 1)
+                else if(GameWinner() == 9)
                     Console.WriteLine("Победили крестики");
-                else if(GameWinner() == 2)
+                else if(GameWinner() == 10)
                     Console.WriteLine("Победили нолики");
+                Console.Clear();
+                ShowBoard();
                 Console.WriteLine("Нажмите любую кнопку, чтобы начать заново...");
                 Console.ReadKey();
                 RestartGame();
